2b10637c808635f194451fc6c1a59ce5c27e2c95
Simone Rancati <simo.ranca03@gmail.com>
2026-07-06 18:53:42 +0200

    Aggiunta generazione GIF

--- src/DQN.py
+++ src/DQN.py
@@ -20,7 +20,7 @@ class BrainTumorEnv(gymnasium.Env):
 
         # Definizione dell'immagine e del box
         self.observation_space = spaces.Dict({
-            "image": spaces.Box(low=0, high=255, shape=(640, 640, 3), dtype=np.uint8),
+            "image": spaces.Box(low=0, high=1, shape=(224, 224, 3), dtype=np.float32),
             "box": spaces.Box(low=0, high=1, shape=(4,), dtype=np.float32)
         })
 
@@ -39,7 +39,7 @@ class BrainTumorEnv(gymnasium.Env):
 
         # Definiamo una box iniziale al centro dell'immagine e di dimensione pari al 100% della stessa
         # TODO: diminuire dimensione box
-        box_size = 1.0
+        box_size = 0.7
         start_x = 0.5 - (box_size / 2)
         start_y = 0.5 - (box_size / 2)
         
@@ -130,7 +130,6 @@ class BrainTumorEnv(gymnasium.Env):
 
         return interArea / float(boxAArea + boxBArea - interArea + 1e-6)
 
-    # TODO: calcolare la reward in base al passo precedente
     def calculate_reward(self):
         # Calcolo la IoU sulla box determinata dall'agente e quella target
         target = self.target_boxes[self.current_img_idx]
@@ -171,20 +170,15 @@ class BrainTumorEnv(gymnasium.Env):
         self.prev_dist = dist
 
         # Penalizzazione per ogni passo aggiuntivo
-        reward -= 0.01
+        reward -= 0.02
         
         return reward
 
 def build_dqn_model():
-    # Prendo in input l'immagine
-    img_input = tf.keras.layers.Input(shape=(640, 640, 3), name="image")
+    img_input = tf.keras.layers.Input(shape=(224, 224, 3), name="image")
 
-    # Riduco la dimensione dell'immaigne e normalizzo i pixel
-    x = tf.keras.layers.Resizing(224, 224)(img_input)
-    x = tf.keras.layers.Rescaling(1./255)(x) 
-
-    # Estrazione delle feature dall'immagine ridotta
-    x = tf.keras.layers.Conv2D(32, 3, activation='relu')(x)
+    # Estrazione delle feature dall'immagine
+    x = tf.keras.layers.Conv2D(32, 3, activation='relu')(img_input)
     x = tf.keras.layers.MaxPooling2D()(x)
     x = tf.keras.layers.Conv2D(64, 3, activation='relu')(x)
     x = tf.keras.layers.MaxPooling2D()(x)
@@ -203,10 +197,7 @@ def build_dqn_model():
     dense = tf.keras.layers.Dense(64, activation='relu')(dense)
     output = tf.keras.layers.Dense(8, activation='linear')(dense)
 
-    # Uso Adam come ottimizzatore
-    # TODO: variare LR e vedere performance
     model = tf.keras.Model(inputs=[img_input, box_input], outputs=output)
-    model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.0007), loss='mse')
     return model
 
 class DQNAgent:
@@ -215,7 +206,7 @@ class DQNAgent:
 
         # Definisce la dimensione della memoria delle ultime esperienze
         # TODO: testare aumento della memoria
-        self.memory = deque(maxlen=500) 
+        self.memory = deque(maxlen=10000) 
 
         # Quanto conta il futuro rispetto ad adesso
         self.gamma = 0.95
@@ -227,11 +218,12 @@ class DQNAgent:
         self.epsilon_min = 0.01
 
         # Diminuzione dopo ogni allenamento (viene moltiplicato ogni volta)
-        self.epsilon_decay = 0.9975
+        self.epsilon_decay = 0.9985
 
         # Crea la rete neurale
         self.model = build_dqn_model()
         self.target_model = build_dqn_model()
+        self.optimizer = tf.keras.optimizers.Adam(learning_rate=0.0007)
         self.update_target_model()
 
     def update_target_model(self):
@@ -243,9 +235,10 @@ class DQNAgent:
             return random.randrange(self.action_size)
         
         # Scelgo l'azione che ha valore massimo in base al modello
+        # OTTIMIZZAZIONE: Usato model() invece di predict()
         img = np.expand_dims(state['image'], axis=0)
         box = np.expand_dims(state['box'], axis=0)
-        act_values = self.model.predict([img, box], verbose=0)
+        act_values = self.model([img, box], training=False)
         return np.argmax(act_values[0])
 
     def remember(self, state, action, reward, next_state, done):
@@ -259,24 +252,34 @@ class DQNAgent:
         # Estraggo un minibatch
         minibatch = random.sample(self.memory, batch_size)
 
-        states_img = np.array([s['image'] for s, a, r, ns, d in minibatch])
-        states_box = np.array([s['box'] for s, a, r, ns, d in minibatch])
-        next_states_img = np.array([ns['image'] for s, a, r, ns, d in minibatch])
-        next_states_box = np.array([ns['box'] for s, a, r, ns, d in minibatch])
-        rewards = np.array([r for s, a, r, ns, d in minibatch])
+        states_img = np.array([s['image'] for s, a, r, ns, d in minibatch], dtype=np.float32)
+        states_box = np.array([s['box'] for s, a, r, ns, d in minibatch], dtype=np.float32)
+        next_states_img = np.array([ns['image'] for s, a, r, ns, d in minibatch], dtype=np.float32)
+        next_states_box = np.array([ns['box'] for s, a, r, ns, d in minibatch], dtype=np.float32)
+        rewards = np.array([r for s, a, r, ns, d in minibatch], dtype=np.float32)
         actions = np.array([a for s, a, r, ns, d in minibatch])
         dones = np.array([d for s, a, r, ns, d in minibatch])
 
-        targets = self.model.predict([states_img, states_box], verbose=0)
-        next_q_values = self.target_model.predict([next_states_img, next_states_box], verbose=0)
+        # OTTIMIZZAZIONE: Uso di GradientTape invece di model.fit
+        with tf.GradientTape() as tape:
+            # Calcolo i valori Q attuali e futuri
+            targets = self.model([states_img, states_box], training=True)
+            next_q_values = self.target_model([next_states_img, next_states_box], training=False)
+            
+            # Calcolo il target per l'azione scelta
+            max_next_q = tf.reduce_max(next_q_values, axis=1)
+            updated_q_values = rewards + (1 - dones) * self.gamma * max_next_q
+            
+            # Creiamo una maschera per aggiornare solo l'azione presa
+            masks = tf.one_hot(actions, self.action_size)
+            
+            # Calcoliamo la loss (MSE) solo per le azioni scelte
+            current_q_actions = tf.reduce_sum(targets * masks, axis=1)
+            loss = tf.reduce_mean(tf.square(updated_q_values - current_q_actions))
+                # Applico i gradienti
+        grads = tape.gradient(loss, self.model.trainable_variables)
+        self.optimizer.apply_gradients(zip(grads, self.model.trainable_variables))
 
-        for i in range(batch_size):
-            target = rewards[i]
-            if not dones[i]:
-                target = (rewards[i] + self.gamma * np.amax(next_q_values[i]))
-            targets[i][actions[i]] = target
-
-        self.model.fit([states_img, states_box], targets, epochs=1, verbose=0)
         if self.epsilon > self.epsilon_min:
             self.epsilon *= self.epsilon_decay
 
@@ -300,7 +303,10 @@ def load_coco_dataset(folder_path):
         if image is None:
             continue
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        image = cv2.resize(image, (640, 640))
+        
+        image = cv2.resize(image, (224, 224))
+        image = image.astype(np.float32) / 255.0
+        
         bbox = ann['bbox'] 
         orig_w, orig_h = 0, 0
         for img_info in coco_data['images']:
@@ -312,50 +318,55 @@ def load_coco_dataset(folder_path):
         images_list.append(image)
         boxes_list.append(norm_bbox)
         processed_images.add(img_id)
-    return np.array(images_list, dtype=np.uint8), np.array(boxes_list, dtype=np.float32)
+    return np.array(images_list, dtype=np.float32), np.array(boxes_list, dtype=np.float32)
 
 def visualize_progress(env, agent, episode):
     num_samples = 3
-    plt.figure(figsize=(15, 5))
+    plt.figure(figsize=(16, 6))
+    
     for i in range(num_samples):
+        # Scegliamo un'immagine casuale per il test
         idx = random.randint(0, len(env.images) - 1)
-        img = env.images[idx]
-        target_box = env.target_boxes[idx]
+        
+        # Resettiamo l'ambiente e forziamo l'indice dell'immagine scelta
+        state, _ = env.reset()
         env.current_img_idx = idx
-        current_box = np.random.rand(4).astype(np.float32)
-        state = {"image": img, "box": current_box}
+        state['image'] = env.images[idx] # Sincronizziamo l'osservazione con l'immagine scelta
+        
+        # Simuliamo l'agente per un numero limitato di passi usando l'ambiente
         for _ in range(50):
             action = agent.act(state, training=False)
-            move_size = 0.01
-            if action == 0: current_box[1] -= move_size 
-            elif action == 1: current_box[1] += move_size 
-            elif action == 2: current_box[0] -= move_size 
-            elif action == 3: current_box[0] += move_size 
-            elif action == 4:
-                current_box[0] -= move_size / 2
-                current_box[2] += move_size
-            elif action == 5:
-                current_box[0] += move_size / 2
-                current_box[2] -= move_size
-            elif action == 6:
-                current_box[1] -= move_size / 2
-                current_box[3] += move_size
-            elif action == 7:
-                current_box[1] += move_size / 2
-                current_box[3] -= move_size
-            current_box = np.clip(current_box, 0, 1)
-            state = {"image": img, "box": current_box}
+            # Usiamo env.step invece di riscrivere la logica dei movimenti
+            next_state, _, terminated, truncated, _ = env.step(action)
+            state = next_state
+            if terminated or truncated:
+                break
+        
+        # Recuperiamo i dati finali per il plotting
+        img = env.images[idx]
+        target_box = env.target_boxes[idx]
+        current_box = state['box']
+
         plt.subplot(1, num_samples, i + 1)
         plt.imshow(img)
-        rect_real = patches.Rectangle((target_box[0]*640, target_box[1]*640), target_box[2]*640, target_box[3]*640, linewidth=2, edgecolor='r', facecolor='none', label='Reale')
-        rect_agent = patches.Rectangle((current_box[0]*640, current_box[1]*640), current_box[2]*640, current_box[3]*640, linewidth=2, edgecolor='g', facecolor='none', label='Agente')
+        
+        # Disegniamo la box reale (rossa) e quella dell'agente (verde)
+        rect_real = patches.Rectangle((target_box[0]*224, target_box[1]*224), 
+                                      target_box[2]*224, target_box[3]*224, 
+                                      linewidth=2, edgecolor='r', facecolor='none', label='Reale')
+        rect_agent = patches.Rectangle((current_box[0]*224, current_box[1]*224), 
+                                       current_box[2]*224, current_box[3]*224, 
+                                       linewidth=2, edgecolor='g', facecolor='none', label='Agente')
+        
         plt.gca().add_patch(rect_real)
         plt.gca().add_patch(rect_agent)
         plt.title(f"Episodio {episode}")
         plt.legend()
         plt.axis('off')
+        
     plt.tight_layout()
-    plt.show()
+    plt.savefig(f"progress_episode_{episode}.png")
+    plt.close()
 
 dataset_path = kagglehub.dataset_download("pkdarabi/brain-tumor-image-dataset-semantic-segmentation")
 train_images, train_boxes = load_coco_dataset(os.path.join(dataset_path, "train"))
@@ -365,9 +376,9 @@ env_train = BrainTumorEnv(train_images, train_boxes)
 env_test = BrainTumorEnv(test_images, test_boxes)
 agent = DQNAgent(action_size=8)
 
-episodes = 2000
+episodes = 4000
 batch_size = 64
-rewards_history = [] # Lista per memorizzare la reward di ogni episodio
+rewards_history = []
 
 for e in range(episodes):
     state, _ = env_train.reset() 
@@ -382,19 +393,16 @@ for e in range(episodes):
         if done:
             break
     
-    rewards_history.append(total_reward) # Salvataggio reward
+    rewards_history.append(total_reward)
     agent.replay(batch_size)
     
     if (e + 1) % 10 == 0:
-        # Aggiorno la target network
         agent.update_target_model()
-
-        # Calcolo la media degli ultimi 10 episodi
         avg_reward = np.mean(rewards_history[-10:])
         print(f"Episodio: {e+1}/{episodes}, Reward: {total_reward:.2f}, Media 10 ep: {avg_reward:.2f}, Epsilon: {agent.epsilon:.2f}")
     
-    # if (e + 1) % 200 == 0:
-        # visualize_progress(env_train, agent, e + 1)
+    if (e + 1) % 200 == 0:
+        visualize_progress(env_train, agent, e + 1)
 
 print("\nInizio Fase di Test...")
 test_episodes = 50
@@ -413,10 +421,9 @@ for e in range(test_episodes):
 
 print(f"Test completato. Success Rate: {(successes/test_episodes)*100:.2f}%")
 
-# Implementazione grafico della reward
 plt.figure(figsize=(10, 5))
 plt.plot(rewards_history, label='Reward per Episodio', color='blue', alpha=0.3)
-# Aggiungo una media mobile per rendere il grafico più leggibile
+
 if len(rewards_history) >= 10:
     moving_avg = np.convolve(rewards_history, np.ones(10)/10, mode='valid')
     plt.plot(range(9, len(rewards_history)), moving_avg, label='Media Mobile (10 ep)', color='red', linewidth=2)
